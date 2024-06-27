0a58fb1d7c04e872d64bda3a8abf598128165b69
ryuncoders <seungjy10@gmail.com>
2024-06-27 17:04:40 +0900

    feat(Data Fetching): Parallel Requests, Using Promise.all()

--- app/(Movies)/movies/[id]/page.tsx
+++ app/(Movies)/movies/[id]/page.tsx
@@ -5,12 +5,19 @@ async function getMovie(id: string) {
   const json = await response.json();
   return json;
 }
+async function getVideos(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  const json = await response.json();
+  return json;
+}
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const movie = await getMovie(id);
+  // const movie = await getMovie(id);
+  // const videos = await getVideos(id);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
   return <div>{movie.title}</div>;
 }
