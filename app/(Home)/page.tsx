a12fdc0dcc3973b6fea989152b1f4d3278c8eb78
ryuncoders <seungjy10@gmail.com>
2024-06-27 17:00:09 +0900

    feat(Data Fetching): Fetch movie detail

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,18 +1,30 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovie() {
   // return await fetch(URL).then((response) => response.json());
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  const response = await fetch(URL);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovie();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- app/(Movies)/movies/[id]/page.tsx
+++ app/(Movies)/movies/[id]/page.tsx
@@ -1,7 +1,16 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(Home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  const json = await response.json();
+  return json;
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>Movie {id}</div>;
+  const movie = await getMovie(id);
+  return <div>{movie.title}</div>;
 }
